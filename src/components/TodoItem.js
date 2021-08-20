78bf9b27ddc0562de48f8f32b2a2982dbc6ec2e6
minna-amin <minna.amin@gmail.com>
2021-08-19 22:20:34 -0400

    added input fields

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,7 +6,11 @@ class TodoItem extends React.Component{
            <input 
            type="checkbox" 
            checked={this.props.todo.completed}
-           onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>{this.props.todo.title}</li>
+           onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
+           
+           <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+
+           {this.props.todo.title}</li>
     }
 }
     
