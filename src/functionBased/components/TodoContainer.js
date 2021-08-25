4f2e31ba66589d1809b1945b27f8f03ee6ad67cb
minna-amin <minna.amin@gmail.com>
2021-08-25 19:43:22 -0400

    converting to func based and added svg icons

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,97 +1,83 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import TodosList from "./TodoList"
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 import { v4 as uuidv4 } from "uuid";
 
-class TodoContainer extends React.Component{
-    state = {
-        todos: [],
-    };
+const TodoContainer = () => {
+  const [todos, setTodos] = useState(getInitialTodos())
 
-    handleChange =  (id) => {
-      this.setState(prevState => ({
-        todos: prevState.todos.map(todo => {
-          if (todo.id === id) {
-            return {
-              ...todo,
-              completed: !todo.completed,
-            }
+  const handleChange = id => {
+    setTodos(prevState =>
+      prevState.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
           }
-          return todo
-        }),
-      }))
-    };
-
-    delTodo = id => {
-      this.setState({
-        todos: [
-          ...this.state.todos.filter(todo => {
-            return todo.id !== id;
-          })
-        ]
-      });
-    };
-
-    addTodoItem = title =>{
-      const newTodo = {
-        id: uuidv4(),
-        title: title,
-        completed: false
-      };
-      this.setState({
-        todos: [...this.state.todos, newTodo]
-      });
-    };
-
-    setUpdate = (updatedTitle, id) => {
-      this.setState({
-        todos: this.state.todos.map(todo => {
-          if (todo.id === id) {
-            todo.title = updatedTitle
-          }
-          return todo
-        }),
+        }
+        return todo
       })
+    )
+  }
+
+  const delTodo = id => {
+    setTodos([
+      ...todos.filter(todo => {
+        return todo.id !== id
+      }),
+    ])
+  }
+
+  const addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
     }
+    setTodos([...todos, newTodo])
+  }
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      })
+    )
+  }
+
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || []
+  }
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
 
 
-    componentDidUpdate(prevProps, prevState) {
-      if(prevState.todos !== this.state.todos) {
-        const temp = JSON.stringify(this.state.todos)
-        localStorage.setItem("todos", temp)
-      }
-    }
-
-    componentDidMount() {
-      const temp = localStorage.getItem("todos")
-      const loadedTodos = JSON.parse(temp)
-      if (loadedTodos) {
-        this.setState({
-          todos: loadedTodos
-        })
-      }
-    }
-
-
-   
-
-    render() {
         return(
           <div className="container">
             <div className="inner">
             <Header />
-            <InputTodo addTodoProps={this.addTodoItem}/>
+            <InputTodo addTodoProps={addTodoItem}/>
             <TodosList 
-            todos={this.state.todos} 
-            handleChangeProps = {this.handleChange} 
-            deleteTodoProps = {this.delTodo} 
-            setUpdate={this.setUpdate}
+            todos={todos} 
+            handleChangeProps = {handleChange} 
+            deleteTodoProps = {delTodo} 
+            setUpdate={setUpdate}
             />
             </div>
           </div>
-        )
-    }
+        
+    )
 }
 
 export default TodoContainer
--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,72 +1,68 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./TodoItem.module.css"
+import { FaTrash } from "react-icons/fa"
 
-class TodoItem extends React.Component{
-    handleEditing  = () => {
-        this.setState({
-            editing: true,
-          })
+const TodoItem = props => {
+  const [editing, setEditing] = useState(false)
+
+  const handleEditing = () => {
+    setEditing(true)
+  }
+
+  const handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      setEditing(false)
     }
+  }
 
-    state = {
-        editing: false,
-      }
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
 
-      handleUpdatedDone = event => {
-        if (event.key === "Enter") {
-            this.setState({ editing: false })
-          }
-      }
+  const { completed, id, title } = props.todo
 
-      componentWillUnmount() {
-        console.log("Cleaning up...")
-      }
+  let viewMode = {}
+  let editMode = {}
 
-      
-    render() {
-        const completedStyle = {
-            fontStyle: "italic",
-            color: "#595959",
-            opacity: 0.4,
-            textDecoration: "line-through",
-          } 
-const { completed, id, title } = this.props.todo
-
-let viewMode = {}
-let editMode ={}
-
-if(this.state.editing) {
-    viewMode.display =  "none"
-}else {
+  if (editing) {
+    viewMode.display = "none"
+  } else {
     editMode.display = "none"
-}
+  }
 
-
-       return <li className={styles.item}>
-           <div onDoubleClick={this.handleEditing} style={viewMode}>
-           <input 
-           type="checkbox" 
-           className={styles.checkbox}
-           checked={completed}
-           onChange={() => this.props.handleChangeProps(id)}/>
-           
-           <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-           <span style={completed ? completedStyle : null}>
-
-           {title}
-           </span>
-           </div>
-           <input type="text" 
-           style={editMode} 
-           className={styles.textInput} 
-           value={title} 
-           onChange={e => {
-            this.props.setUpdate(e.target.value, id)
-           }}
-           onKeyDown={this.handleUpdatedDone}/>
-           </li>
+  useEffect(() => {
+    return () => {
+      console.log("cleaning up")
     }
+  },[])
+
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.deleteTodoProps(id)}><FaTrash style={{ color: "orangered", fontSize: "16px" }}/></button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={e => {
+          props.setUpdate(e.target.value, id)
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  )
 }
-    
 
 export default TodoItem
